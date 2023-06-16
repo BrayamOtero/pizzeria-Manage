47d8b896f52f5d17f2efe3073d0c979922f7cd26
Brayam Otero <davidotero@unicauca.edu.co>
2023-06-16 11:20:21 -0600

    FEAT: Make queries use JDBCTemplate

---
+++ src/main/java/com/otero/pizza/web/controller/PizzaController.java
@@ -0,0 +1,28 @@
+package com.otero.pizza.web.controller;
+
+import com.otero.pizza.persitence.entity.PizzaEntity;
+import com.otero.pizza.service.PizzaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pizzas")
+public class PizzaController {
+
+    private final PizzaService pizzaService;
+
+    @Autowired
+    public PizzaController(PizzaService pizzaService) {
+        this.pizzaService = pizzaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PizzaEntity>> gelAll(){
+        return ResponseEntity.ok(this.pizzaService.getAll());
+    }
+}
