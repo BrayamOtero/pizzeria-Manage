4ced260ed19c98d8c220cabf9c0f77d807e61afd
Brayam Otero <davidotero@unicauca.edu.co>
2023-06-15 18:15:30 -0600

    First commit

---
+++ src/main/java/com/otero/pizza/PizzeriaManageApplication.java
@@ -0,0 +1,13 @@
+package com.otero.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzeriaManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzeriaManageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otero/pizza/PizzeriaManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otero.pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzeriaManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
