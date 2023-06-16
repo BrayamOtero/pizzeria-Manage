983a3c42cc6da2644118c16de7c84ebabda07970
Brayam Otero <davidotero@unicauca.edu.co>
2023-06-16 12:35:15 -0600

    FEAT: Use  http status when pizza already exists in POST and when pizza already don't exist in PUT

--- src/main/java/com/otero/pizza/web/controller/PizzaController.java
+++ src/main/java/com/otero/pizza/web/controller/PizzaController.java
@@ -3,6 +3,7 @@ package com.otero.pizza.web.controller;
 import com.otero.pizza.persitence.entity.PizzaEntity;
 import com.otero.pizza.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,16 +31,16 @@ public class PizzaController {
     }
 
     @PostMapping
-    public ResponseEntity<PizzaEntity> add(@RequestBody PizzaEntity pizza){
+    public ResponseEntity<?> add(@RequestBody PizzaEntity pizza){
         if(pizza.getIdPizza() == null || !this.pizzaService.exists(pizza.getIdPizza()))
             return ResponseEntity.ok(this.pizzaService.save(pizza));
-        return ResponseEntity.badRequest().build();
+        return ResponseEntity.status(HttpStatus.CONFLICT).body("La Pizza ya Existe!");
     }
 
     @PutMapping
-    public ResponseEntity<PizzaEntity> update(@RequestBody PizzaEntity pizza){
+    public ResponseEntity<?> update(@RequestBody PizzaEntity pizza){
         if(pizza.getIdPizza() != null && this.pizzaService.exists(pizza.getIdPizza()))
             return ResponseEntity.ok(this.pizzaService.save(pizza));
-        return ResponseEntity.badRequest().build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("La Pizza no Existe!");
     }
 }
