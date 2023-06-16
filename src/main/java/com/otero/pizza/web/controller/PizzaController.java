34263715e0b9c8ff055d22664f09e487e5252ff2
Brayam Otero <davidotero@unicauca.edu.co>
2023-06-16 12:26:46 -0600

    FEAT: Create and update with jpa

--- src/main/java/com/otero/pizza/web/controller/PizzaController.java
+++ src/main/java/com/otero/pizza/web/controller/PizzaController.java
@@ -4,10 +4,7 @@ import com.otero.pizza.persitence.entity.PizzaEntity;
 import com.otero.pizza.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +27,19 @@ public class PizzaController {
     @GetMapping("/{idPizza}")
     public ResponseEntity<PizzaEntity> get(@PathVariable int idPizza){
         return ResponseEntity.ok(this.pizzaService.get(idPizza));
+    }
+
+    @PostMapping
+    public ResponseEntity<PizzaEntity> add(@RequestBody PizzaEntity pizza){
+        if(pizza.getIdPizza() == null || !this.pizzaService.exists(pizza.getIdPizza()))
+            return ResponseEntity.ok(this.pizzaService.save(pizza));
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<PizzaEntity> update(@RequestBody PizzaEntity pizza){
+        if(pizza.getIdPizza() != null && this.pizzaService.exists(pizza.getIdPizza()))
+            return ResponseEntity.ok(this.pizzaService.save(pizza));
+        return ResponseEntity.badRequest().build();
     }
 }
