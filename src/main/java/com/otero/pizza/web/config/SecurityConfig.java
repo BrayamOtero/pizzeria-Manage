8adc184458ae2b81da7021dcbc51d2a98a7b6798
Brayam Otero <davidotero@unicauca.edu.co>
2023-07-21 21:34:43 -0600

    FEAT: Using requestMatchers to permit the  method GET at /api/pizza and deny the method PUT

--- src/main/java/com/otero/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/otero/pizza/web/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.otero.pizza.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,13 +13,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .csrf(AbstractHttpConfigurer::disable)
                 .cors().and()
-                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> {
-                    authorizationManagerRequestMatcherRegistry
-                    .anyRequest()
-                    .authenticated();
-                })
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry
+                        .requestMatchers(HttpMethod.GET,"/api/pizza/**").permitAll()
+                        .requestMatchers(HttpMethod.PUT).denyAll()
+                        .anyRequest()
+                        .authenticated())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
     }
