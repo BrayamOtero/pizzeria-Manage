9afbd288267831801447ac87e4c8e966c121a88c
Brayam Otero <davidotero@unicauca.edu.co>
2023-07-23 11:06:10 -0600

    FEAT: create table of roles and get from DB

--- src/main/java/com/otero/pizza/service/UserSecurityService.java
+++ src/main/java/com/otero/pizza/service/UserSecurityService.java
@@ -1,6 +1,7 @@
 package com.otero.pizza.service;
 
 import com.otero.pizza.persitence.entity.UserEntity;
+import com.otero.pizza.persitence.entity.UserRoleEntity;
 import com.otero.pizza.persitence.repositoy.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
@@ -23,10 +24,12 @@ public class UserSecurityService implements UserDetailsService {
         UserEntity user = this.userRepository.findById(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User : " + username + " not found."));
 
+        String[] roles = user.getRoles().stream().map(UserRoleEntity::getRole).toArray(String[]::new);
+
         return User.builder()
                 .username(user.getUsername())
                 .password(user.getPassword())
-                .roles("ADMIN")
+                .roles(roles)
                 .accountLocked(user.getLocked())
                 .disabled(user.getDisabled())
                 .build();
